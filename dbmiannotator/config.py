04901ef0666c5ca10eb6556579630eecb87f93d7
rkboyce <codehop.dev@gmail.com>
2015-11-25 14:39:30 -0500

    test

--- dbmiannotator/config.py
+++ dbmiannotator/config.py
@@ -1,2 +1,2 @@
 ## Mysql connection config
-SQLALCHEMY_DATABASE_URI = 'mysql://<username>:<password>@localhost/AnnotatorDBMI'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:5bboys@localhost/AnnotatorDBMI'
