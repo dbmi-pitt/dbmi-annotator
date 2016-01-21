1987e49b5e2b4d9402d1e3c8c3a0d766027cb25c
wenzhang61 <wez61@pitt.edu>
2016-01-21 16:33:39 -0500

    add pdf splitter

---
+++ public/js/dbmi.js
@@ -0,0 +1,53 @@
+﻿$(document).ready(function () {
+    $('#splitter').jqxSplitter({ width: 1270, height: '100%',  panels: [{ size: '100%', min: 100 }, { size: '0%', min: 0}] });
+  });
+
+//show right splitter by button "btn-success"
+  function showrightbyvalue(){
+    
+    if($('.btn-success').val()=="hide") {
+      
+      $('#splitter').jqxSplitter({
+        width: 1270,
+        height: '100%',
+        panels: [{size: '67%', min: 850}, {size: '33%', min: 410}]
+      });
+      $('.btn-success').val("show");
+      $('.annotator-editor').show();
+      $('.btn-success').css("margin-right",420);
+    }
+    else {
+      $('#splitter').jqxSplitter({
+        width: 1270,
+        height: '100%',
+        panels: [{size: '100%', min: 100}, {size: '0%', min: 0}]
+      });
+      $('.btn-success').val("hide");
+      $('.annotator-editor').hide();
+      $('.btn-success').css("margin-right",0);
+    }
+
+  }
+
+//show right splitter directly
+  function showright(){
+    
+      $('#splitter').jqxSplitter({
+        width: 1270,
+        height: '100%',
+        panels: [{size: '67%', min: 850}, {size: '33%', min: 410}]
+      });
+      $('.btn-success').val("show");
+      $('.annotator-editor').show();
+      $('.btn-success').css("margin-right",420);
+    /*}
+    else {
+      $('#splitter').jqxSplitter({
+        width: 1270,
+        height: '100%',
+        panels: [{size: '100%', min: 100}, {size: '0%', min: 0}]
+      });
+      $('.btn-success').val("hide");
+    }*/
+
+  }
