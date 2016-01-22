065849e40c34d131da82d931280fa0d2b8611336
ningyifan <ningyifan1212@163.com>
2016-01-22 11:42:33 -0500

    pdf image extraction via nodejs request

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 
 var app = express();
 
-_dirname = "/Users/wenzhang/GitHub/dbmi-annotator/";
+_dirname = "/home/yin2/dbmi-annotator/";
 
 // Register all our routes
 app.use(express.static(_dirname + 'public'));
@@ -12,3 +12,33 @@ var router = express.Router();
 
 // Start the server
 app.listen(3000);
+
+app.get('/extractImages', function(req, res) {
+    
+    var pdfdoc = req.param('pdfdoc');
+    
+    res.send('pdfdoc: ' + pdfdoc);
+    console.log("[INFO] server received request with doc: " + pdfdoc)
+
+    // run pdfimages
+    var sys = require('sys')
+    var exec = require('child_process').exec;
+
+    function puts(error, stdout, stderr) { sys.puts(stdout) }
+    
+    exec("mkdir pdf-images/images-" + pdfdoc);
+    exec("pdfimages -j public/DDI-pdfs/" +pdfdoc + " pdf-images/images-" + pdfdoc +"/", puts);
+    
+    console.log("[INFO] extraction complete!");
+
+    
+});
+
+
+// app.get('/viewer.html', function(req, res) {
+
+//     var pdfdoc = req.param('file');
+    
+//     res.send('pdfdoc: ' + pdfdoc);
+//     console.log("server received request in viewer with doc: " + pdfdoc)
+// });
