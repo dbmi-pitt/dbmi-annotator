c2d72830b6adb82fa1d18ad4a29334335d8c2992
Yifan Ning <ningyifan1212@163.com>
2016-03-25 17:56:48 -0400

    update pre-annotation tool

--- pre-annot-tool/load-ner.js
+++ pre-annot-tool/load-ner.js
@@ -1,27 +1,27 @@
-//var NER_RESULTS = "ners-03012016.json";
+
 var NER_RESULTS = "ner-resutls-json";
 var ES_CONN = "localhost:9250";
 var HOSTNAME = "localhost";
 
 var uuid = require('uuid');
+var USER_EMAIL = "yin2@gmail.com"
 
 var elasticsearch = require('elasticsearch');
 var client = new elasticsearch.Client({
-  host: ES_CONN,
-  log: 'trace'
+  host: ES_CONN
+  //log: 'trace'
 });
+
 
 var fs = require('fs');
 var nerResults = fs.readFileSync(NER_RESULTS,"utf-8");
-
 var nersets = JSON.parse(nerResults).nersets;
 
-for (i = 0; i < nersets.length; i++){
-//for (i = 0; i < 1; i++){
+//for (i = 0; i < nersets.length; i++){
+for (i = 0; i < 1; i++){
     subL = nersets[i];
 
     for (j = 0; j < subL.length; j++){
-    //for (j = 0; j < 80; j++){
         annot = subL[j];
 
         if (annot){
@@ -41,55 +41,91 @@ function es_index(annot){
         return null;
     } else {
 
-        uriStr = "http://" + HOSTNAME + "/nlabels/" + annot.setid + ".html";
+        //uriStr = "http://" + HOSTNAME + "/nlabels/" + annot.setid + ".html";
+        uriStr = "http://" + HOSTNAME + "/DDI-labels/" + annot.setid + ".html";
         uriPost = uriStr.replace(/[\/\\\-\:\.]/g, "");
 
-        //path = "/article[1]/div[1]/div[1]/div[1]" + annot.start
-        path = annot.start.replace("/html[1]/body[1]","/article[1]/div[1]/div[1]/div[1]");
-        var datetime = new Date();
-        client.index(
+        //path = annot.start.replace("/html[1]/body[1]","/article[1]/div[1]/div[1]/div[1]");
+        path = annot.start.replace("/html[1]/body[1]","/article[1]/div[1]/div[1]");
+
+        var isExists = false;
+        console.log("[INFO]: begin check for " + annot.exact);
+
+        client.search(
             {
                 index: 'annotator',
                 type: 'annotation',
-                id: uuid.v4(),
-                body: {    // annotatorJs fields for drug Mention
-                    "email": "yin2@gmail.com",
-                    "created": datetime, 
-                    "updated": datetime, 
-                    "annotationType": "DrugMention",
-                    "quote": annot.drugname,
-                    "permissions": {},
-                    "ranges": [
-                        {
-                            "start": path,
-                            "end": path,
-                            "startOffset": parseInt(annot.startOffset),
-                            "endOffset": parseInt(annot.endOffset),
+                body:{
+                    query : {
+                        match: {
+                            "email": USER_EMAIL                            
+                        },
+                        match: {
+                            "uri": uriPost                            
+                        },
+                        match: {
+                            "prefix": annot.prefix
+                        },
+                        match: {
+                            "exact": annot.exact                            
                         }
-                    ],
-                    "consumer": "mockconsumer",
-                    "uri": uriPost,
-                    "rawurl": uriStr,
-                    "user": "NER"
-                },
-                target: {   //for OA text quote selector (JSON-LD)
-                    "source" : uriStr,
-                    "selector" : {
-                        "@type": "TextQuoteSelector",
-                        "exact": "",
-                        "prefix": "",
-                        "suffix": ""
                     }
                 }
-                
-                
-            }, function (err, resp) {
-                if (err)
-                    console.log("[ERROR] " + err)
-                else
-                    console.log("[INFO] load successfully")
-            });
 
+            }).then(function (resp){
+                var hits = resp.hits.hits;
+                console.log("results:" + hits.length);
+                
+                if (hits.length > 0)
+                    console.log("[EXITS]");
+                else {
+                    console.log("[NOT EXITS]");
+                    console.log("[INFO]: begin load for " + annot.exact);
+                    var datetime = new Date();
+                    client.index(
+                        {
+                            index: 'annotator',
+                            type: 'annotation',
+                            id: uuid.v4(),
+                            body: {    // annotatorJs fields for drug Mention
+                                "email": USER_EMAIL,
+                                "created": datetime, 
+                                "updated": datetime, 
+                                "annotationType": "DrugMention",
+                                "quote": annot.drugname,
+                                "permissions": {},
+                                "ranges": [
+                                    {
+                                        "start": path,
+                                        "end": path,
+                                        "startOffset": parseInt(annot.startOffset),
+                                        "endOffset": parseInt(annot.endOffset),
+                                    }
+                                ],
+                                "consumer": "mockconsumer",
+                                "uri": uriPost,
+                                "rawurl": uriStr,
+                                "user": "NER",
+                                target: {   //for OA text quote selector (JSON-LD)
+                                    "source" : uriStr,
+                                    "selector" : {
+                                        "@type": "TextQuoteSelector",
+                                        "exact": annot.exact,
+                                        "prefix": annot.prefix,
+                                    "suffix": annot.suffix
+                                    }
+                                }
+                            }
+                        }, function (err, resp) {
+                            if (err)
+                                console.log("[ERROR] " + err)
+                            else
+                                console.log("[INFO] load successfully")
+                        });
+                }
+            }, function (err) {
+                console.log(err);
+            });
     }
 }
 
