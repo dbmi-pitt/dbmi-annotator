5af506984af2ef5327115d11d705c3fd6e39c260
Yu Zhang <louisa78909@gmail.com>
2016-08-23 15:29:09 -0400

    update sql

--- db-script/mp-evidence/mp_evidence_schema_08202016.sql
+++ db-script/mp-evidence/mp_evidence_schema_08202016.sql
@@ -1,5 +1,5 @@
 -- Database: mpevidenceCREATE DATABASE mpevidence  WITH OWNER = postgres       ENCODING = 'UTF8'       TABLESPACE = pg_default       LC_COLLATE = 'en_US.UTF-8'       LC_CTYPE = 'en_US.UTF-8'CONNECTION LIMIT = -1;--TABLE: mp_claim_annotation
-DROP TABLE IF EXISTS mp_claim_annotation;
+DROP TABLE IF EXISTS mp_claim_annotation CASCADE;
 CREATE TABLE mp_claim_annotation
 (
 id INTEGER not null PRIMARY KEY,
@@ -14,7 +14,7 @@ date_updated date
 
 
 --TABLE: oa_claim_body
-DROP TABLE IF EXISTS oa_claim_body;
+DROP TABLE IF EXISTS oa_claim_body CASCADE;
 CREATE TABLE oa_claim_body
 (
 id INTEGER not null PRIMARY KEY,
@@ -22,12 +22,12 @@ urn text,
 label text,
 claim_text text,
 np_assertion_id integer,
-is_oa_body_of integer
+is_oa_body_of integer,
 );
 
 
 --TABLE: oa_target
-DROP TABLE IF EXISTS oa_target;
+DROP TABLE IF EXISTS oa_target CASCADE;
 CREATE TABLE oa_target
 (
 id INTEGER not null PRIMARY KEY,
@@ -39,7 +39,7 @@ has_selector integer
 
 
 --TABLE mp_data_annotation
-DROP TABLE IF EXISTS mp_data_annotation;
+DROP TABLE IF EXISTS mp_data_annotation CASCADE;
 CREATE TABLE mp_data_annotation
 (
 id INTEGER not null PRIMARY KEY,
@@ -51,12 +51,13 @@ creator text,
 mp_claim_id integer,
 mp_data_index integer,
 ev_supports boolean,
+date_created date,
 FOREIGN KEY (mp_claim_id) REFERENCES mp_claim_annotation (id)
 );
 
 
 --TABLE oa_data_body
-DROP TABLE IF EXISTS oa_data_body;
+DROP TABLE IF EXISTS oa_data_body CASCADE;
 CREATE TABLE oa_data_body
 (
 id INTEGER not null PRIMARY KEY,
@@ -71,7 +72,7 @@ FOREIGN KEY (mp_data_annotation_id) REFERENCES mp_data_annotation (id)
 
 
 --TABLE data_field
-DROP TABLE IF EXISTS data_field;
+DROP TABLE IF EXISTS data_field CASCADE;
 CREATE TABLE data_field
 (
 id INTEGER not null PRIMARY KEY,
@@ -85,7 +86,7 @@ FOREIGN KEY (data_body_id) REFERENCES oa_data_body(id)
 
 
 --TABLE mp_material_annotation
-DROP TABLE IF EXISTS mp_material_annotation;
+DROP TABLE IF EXISTS mp_material_annotation CASCADE;
 CREATE TABLE mp_material_annotation
 (
 id INTEGER not null PRIMARY KEY,
@@ -97,12 +98,13 @@ creator text,
 mp_claim_id integer,
 mp_data_index integer,
 ev_supports boolean,
+date_created date,
 FOREIGN KEY (mp_claim_id) REFERENCES mp_claim_annotation(id)
 );
 
 
 --TABLE oa_material_body
-DROP TABLE IF EXISTS oa_material_body;
+DROP TABLE IF EXISTS oa_material_body CASCADE;
 CREATE TABLE oa_material_body
 (
 id INTEGER not null PRIMARY KEY,
@@ -117,7 +119,7 @@ FOREIGN KEY (mp_material_annotation_id) REFERENCES mp_material_annotation(id)
 
 
 --TABLE material_field
-DROP TABLE IF EXISTS material_field;
+DROP TABLE IF EXISTS material_field CASCADE;
 CREATE TABLE material_field
 (
 id INTEGER not null PRIMARY KEY,
@@ -131,12 +133,13 @@ FOREIGN KEY (material_body_id) REFERENCES oa_material_body(id)
 
 
 --TABLE qualifier
-DROP TABLE IF EXISTS qualifier;
+DROP TABLE IF EXISTS qualifier CASCADE;
 CREATE TABLE qualifier
 (
 id INTEGER not null PRIMARY KEY,
 urn text,
 claim_body_id integer,
+qvalue text,
 subject boolean,
 predicate boolean,
 object boolean,
@@ -147,7 +150,7 @@ FOREIGN KEY (claim_body_id) REFERENCES mp_claim_annotation(id)
 
 
 --TABLE method
-DROP TABLE IF EXISTS method;
+DROP TABLE IF EXISTS method CASCADE;
 CREATE TABLE method
 (
 id INTEGER not null PRIMARY KEY,
@@ -160,7 +163,7 @@ FOREIGN KEY (mp_data_material_id) REFERENCES mp_material_annotation(id)
 
 
 --TABLE oa_selector
-DROP TABLE IF EXISTS oa_selector;
+DROP TABLE IF EXISTS oa_selector CASCADE;
 CREATE TABLE oa_selector
 (
 id INTEGER not null PRIMARY KEY,
@@ -173,7 +176,7 @@ suffix text
 
 
 --TABLE mp_reference
-DROP TABLE IF EXISTS mp_reference;
+DROP TABLE IF EXISTS mp_reference CASCADE;
 CREATE TABLE mp_reference
 (
 id INTEGER not null PRIMARY KEY,
@@ -185,7 +188,7 @@ date date
 
 
 --TABLE claim_reference_relationships
-DROP TABLE IF EXISTS claim_reference_relationship;
+DROP TABLE IF EXISTS claim_reference_relationship CASCADE;
 CREATE TABLE claim_reference_relationship
 (
 id INTEGER not null PRIMARY KEY,
