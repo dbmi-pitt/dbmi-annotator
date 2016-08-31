9926343ff082dcd024db062d1e22de0346d39b6d
Wen Zhang <wez61@pitt.edu>
2016-08-31 15:26:41 -0400

    load csv to RDB (#144)
    * load csv to RDB
    
    * load csv to RDB
    
    * load csv to RDB

--- translation/mp-evidence/mp_evidence_schema_08202016.sql
+++ translation/mp-evidence/mp_evidence_schema_08202016.sql
@@ -1,15 +1,24 @@
--- Database: mpevidenceCREATE DATABASE mpevidence  WITH OWNER = postgres       ENCODING = 'UTF8'       TABLESPACE = pg_default       LC_COLLATE = 'en_US.UTF-8'       LC_CTYPE = 'en_US.UTF-8'CONNECTION LIMIT = -1;--TABLE: mp_claim_annotation
+-- Database: mpevidence
+CREATE DATABASE mpevidence
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+CONNECTION LIMIT = -1;
+
+
+--TABLE: mp_claim_annotation
 DROP TABLE IF EXISTS mp_claim_annotation CASCADE;
 CREATE TABLE mp_claim_annotation
 (
 id INTEGER not null PRIMARY KEY,
 urn text,
-type text,
 has_body integer,
 has_target integer,
 creator text,
-date_created date,
-date_updated date
+date_created timestamp,
+date_updated timestamp
 );
 
 
@@ -32,7 +41,6 @@ CREATE TABLE oa_target
 (
 id INTEGER not null PRIMARY KEY,
 urn text,
-label text,
 has_source text,
 has_selector integer
 );
@@ -51,7 +59,7 @@ creator text,
 mp_claim_id integer,
 mp_data_index integer,
 ev_supports boolean,
-date_created date,
+date_created timestamp,
 FOREIGN KEY (mp_claim_id) REFERENCES mp_claim_annotation (id)
 );
 
@@ -62,12 +70,11 @@ CREATE TABLE oa_data_body
 (
 id INTEGER not null PRIMARY KEY,
 urn text,
-mp_data_annotation_id integer,
 data_type text,
 vocabulary_id integer,
 concept_code text,
 is_oa_body_of integer,
-FOREIGN KEY (mp_data_annotation_id) REFERENCES mp_data_annotation (id)
+FOREIGN KEY (is_oa_body_of) REFERENCES mp_data_annotation (id)
 );
 
 
@@ -80,7 +87,7 @@ urn text,
 data_body_id integer,
 data_field_type text,
 value_as_string text,
-value_as_number integer,
+value_as_number numeric(10,2),
 FOREIGN KEY (data_body_id) REFERENCES oa_data_body(id)
 );
 
@@ -98,7 +105,7 @@ creator text,
 mp_claim_id integer,
 mp_data_index integer,
 ev_supports boolean,
-date_created date,
+date_created timestamp,
 FOREIGN KEY (mp_claim_id) REFERENCES mp_claim_annotation(id)
 );
 
@@ -109,12 +116,11 @@ CREATE TABLE oa_material_body
 (
 id INTEGER not null PRIMARY KEY,
 urn text,
-mp_material_annotation_id integer,
 material_type text,
 vocabulary_id integer,
 concept_code text,
 is_oa_body_of integer,
-FOREIGN KEY (mp_material_annotation_id) REFERENCES mp_material_annotation(id)
+FOREIGN KEY (is_oa_body_of) REFERENCES mp_material_annotation(id)
 );
 
 
@@ -127,7 +133,7 @@ urn text,
 material_body_id integer,
 material_field_type text,
 value_as_string text,
-value_as_number integer,
+value_as_number numeric(10,2),
 FOREIGN KEY (material_body_id) REFERENCES oa_material_body(id)
 );
 
@@ -140,12 +146,12 @@ id INTEGER not null PRIMARY KEY,
 urn text,
 claim_body_id integer,
 qvalue text,
-subject boolean,
-predicate boolean,
-object boolean,
+subject boolean DEFAULT FALSE,
+predicate boolean DEFAULT FALSE,
+object boolean DEFAULT FALSE,
 concept_code text,
 vocabulary_id integer,
-FOREIGN KEY (claim_body_id) REFERENCES mp_claim_annotation(id)
+FOREIGN KEY (claim_body_id) REFERENCES oa_claim_body(id)
 );
 
 
@@ -156,9 +162,8 @@ CREATE TABLE method
 id INTEGER not null PRIMARY KEY,
 value text,
 mp_claim_id integer,
-mp_data_material_id integer,
-FOREIGN KEY (mp_claim_id) REFERENCES mp_claim_annotation(id),
-FOREIGN KEY (mp_data_material_id) REFERENCES mp_material_annotation(id)
+mp_data_index integer,
+FOREIGN KEY (mp_claim_id) REFERENCES mp_claim_annotation(id)
 );
 
 
@@ -183,11 +188,11 @@ id INTEGER not null PRIMARY KEY,
 urn text,
 reference text,
 author text,
-date date
+reference_date timestamp
 );
 
 
---TABLE claim_reference_relationships
+--TABLE claim_reference_relationship
 DROP TABLE IF EXISTS claim_reference_relationship CASCADE;
 CREATE TABLE claim_reference_relationship
 (
@@ -197,3 +202,46 @@ mp_reference_id integer,
 FOREIGN KEY (mp_claim_id) REFERENCES mp_claim_annotation(id),
 FOREIGN KEY (mp_reference_id) REFERENCES mp_reference(id)
 );
+
+
+CREATE SEQUENCE mp_claim_annotation_id_seq;
+ALTER TABLE mp_claim_annotation alter id set default nextval('mp_claim_annotation_id_seq');
+
+CREATE SEQUENCE oa_selector_id_seq;
+ALTER TABLE oa_selector alter id set default nextval('oa_selector_id_seq');
+
+CREATE SEQUENCE oa_target_id_seq;
+ALTER TABLE oa_target alter id set default nextval('oa_target_id_seq');
+
+CREATE SEQUENCE oa_claim_body_id_seq;
+ALTER TABLE oa_claim_body alter id set default nextval('oa_claim_body_id_seq');
+
+CREATE SEQUENCE qualifier_id_seq;
+ALTER TABLE qualifier alter id set default nextval('qualifier_id_seq');
+
+CREATE SEQUENCE mp_data_annotation_id_seq;
+ALTER TABLE mp_data_annotation alter id set default nextval('mp_data_annotation_id_seq');
+
+CREATE SEQUENCE oa_data_body_id_seq;
+ALTER TABLE oa_data_body alter id set default nextval('oa_data_body_id_seq');
+
+CREATE SEQUENCE data_field_id_seq;
+ALTER TABLE data_field alter id set default nextval('data_field_id_seq');
+
+CREATE SEQUENCE mp_material_annotation_id_seq;
+ALTER TABLE mp_material_annotation alter id set default nextval('mp_material_annotation_id_seq');
+
+CREATE SEQUENCE oa_material_body_id_seq;
+ALTER TABLE oa_material_body alter id set default nextval('oa_material_body_id_seq');
+
+CREATE SEQUENCE material_field_id_seq;
+ALTER TABLE material_field alter id set default nextval('material_field_id_seq');
+
+CREATE SEQUENCE method_id_seq;
+ALTER TABLE method alter id set default nextval('method_id_seq');
+
+CREATE SEQUENCE claim_reference_relationship_id_seq;
+ALTER TABLE claim_reference_relationship alter id set default nextval('claim_reference_relationship_id_seq');
+
+CREATE SEQUENCE mp_reference_id_seq;
+ALTER TABLE mp_reference alter id set default nextval('mp_reference_id_seq');
