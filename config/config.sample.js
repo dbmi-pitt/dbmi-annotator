93326c8351dccb020c993762b3ce2a691c851ba2
Yifan Ning <ningyifan1212@163.com>
2016-11-22 14:34:13 -0500

    add apache2 config to sample

--- config/config.sample.js
+++ config/config.sample.js
@@ -3,14 +3,18 @@ config.store = {};
 config.annotator = {};
 config.elastico = {};
 config.profile = {};
+config.apache2 = {};
 
 // postgres db
 config.postgres = process.env.DATABASE_URL || 'postgres://postgres:<password>@localhost:5432/dbmiannotator'; 
 
+// apache2 
+config.apache2.host = 'localhost';
+config.apache2.port = '80'
+
 // dbmi annotator 
 config.annotator.host = 'localhost';
 config.annotator.port = '3000';
-
 
 // annotator store
 config.store.host = 'localhost';
@@ -18,7 +22,7 @@ config.store.port = '5000';
 
 // elastico
 config.elastico.host = 'localhost';
-config.elastico.port = '9250';
+config.elastico.port = '9200';
 
 // user default profile
 config.profile.def = 'MP';
