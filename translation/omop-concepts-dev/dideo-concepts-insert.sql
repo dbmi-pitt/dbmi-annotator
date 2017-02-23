53740b3f92f69078219bd96fe53bf2a8e037ca16
Yifan Ning <ningyifan1212@163.com>
2017-02-23 11:04:08 -0500

    add dideo uri doc

---
+++ translation/omop-concepts-dev/dideo-concepts-insert.sql
@@ -0,0 +1,4 @@
+-- dideo 
+
+-- TABLE CONCEPT
+INSERT INTO concept (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept , concept_code, valid_start_date, valid_end_date, invalid_reason) VALUES (-9900001, 'interact_with', '', '', '', '', '', '', '', '')
