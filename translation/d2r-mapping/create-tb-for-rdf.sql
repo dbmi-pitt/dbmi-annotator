ba483e1a1b668cc703830a3f7224769544ffe8f8
Yifan Ning <ningyifan1212@163.com>
2017-04-10 16:47:26 -0400

    merge with public-release - add dideo owl to d2r mapping

--- translation/d2r-mapping/create-tb-for-rdf.sql
+++ translation/d2r-mapping/create-tb-for-rdf.sql
@@ -4,6 +4,52 @@ SET SCHEMA 'ohdsi';
 
 CREATE EXTENSION "uuid-ossp";
 
+-- MP Claim and qualifier table: rdf_mp_claim_qualifier --------------------------------
+CREATE TABLE rdf_mp_claim_qualifier AS 
+SELECT 
+mp_claim_id, mp_data_index, method, precipitant, p_concept_code, p_role_concept_code, object, o_concept_code, o_role_concept_code, uuid_generate_v4() as asrt_description_urn, 
+uuid_generate_v4() as p_bearer_role_urn, uuid_generate_v4() as o_bearer_role_urn, uuid_generate_v4() as p_drug_role_urn, uuid_generate_v4() as o_drug_role_urn, uuid_generate_v4() as method_urn, 
+uuid_generate_v4() as p_scattered_molecular_aggregate_urn, uuid_generate_v4() as p_active_ingredient_urn, uuid_generate_v4() as p_drug_urn, uuid_generate_v4() as p_mass_urn, uuid_generate_v4() as p_milligram_urn, 
+uuid_generate_v4() as o_scattered_molecular_aggregate_urn, uuid_generate_v4() as o_active_ingredient_urn, uuid_generate_v4() as o_drug_urn, uuid_generate_v4() as o_mass_urn, uuid_generate_v4() as o_milligram_urn
+FROM (
+WITH method AS (
+SELECT mp_claim_id, mp_data_index, m.entered_value as method from method m
+WHERE m.mp_claim_id = '1'),
+p AS (
+SELECT ca.id, q.qvalue, q.concept_code, q.vocabulary_id, q.qualifier_role_concept_code, cb.label
+FROM mp_claim_annotation ca 
+JOIN oa_claim_body cb on cb.is_oa_body_of = ca.id
+JOIN qualifier q on q.claim_body_id = cb.id
+WHERE q.subject = True
+AND cb.label LIKE '%interact with%'
+AND ca.id = '1'),
+o AS (
+SELECT ca.id, q.qvalue, q.concept_code, q.vocabulary_id, q.qualifier_role_concept_code, cb.label
+FROM mp_claim_annotation ca 
+JOIN oa_claim_body cb on cb.is_oa_body_of = ca.id
+JOIN qualifier q on q.claim_body_id = cb.id
+WHERE q.object = True
+AND cb.label LIKE '%interact with%'
+AND ca.id = '1')
+SELECT method.mp_claim_id, method.mp_data_index, method.method, p.qvalue as precipitant, p.concept_code as p_concept_code, p.qualifier_role_concept_code as p_role_concept_code, o.qvalue as object, 
+o.concept_code as o_concept_code, o.qualifier_role_concept_code as o_role_concept_code
+from method
+JOIN p ON p.id = method.mp_claim_id
+JOIN o ON o.id = method.mp_claim_id
+) AS tb
+
+
+-- MP data table: rdf_mp_data ----------------------------------------------------------
+CREATE TABLE rdf_mp_data AS 
+(SELECT d.mp_claim_id, d.mp_data_index, d.type as datatype, df.type as dftype, df.value, df.direction, uuid_generate_v4() as data_item_urn, uuid_generate_v4() as data_type_urn
+FROM crosstab('SELECT df.data_body_id, df.data_field_type, df.value_as_string FROM data_field df ORDER BY 1,2')
+AS df(data_body_id INTEGER, direction TEXT, type TEXT, value TEXT)
+JOIN oa_data_body db ON df.data_body_id = db.id
+JOIN mp_data_annotation d ON db.id = d.has_body)
+
+
+-- AUC fold RDF creation (DDI clinical trial, interact) --------------------------------
+-- https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxkZGlrcmFuZGlyfGd4OjIzYTc2YjA4NjRkNTAzZDQ
 CREATE TABLE ct_iw_auc_fold AS SELECT 
 method, precipitant, precipitant_code, object, object_code, ratio, ratio_type, value, direction, uuid_generate_v4() as auc_urn, uuid_generate_v4() as fold_urn, uuid_generate_v4() as description_urn, uuid_generate_v4() as bearer_pt_urn, uuid_generate_v4() AS bearer_obj_urn, uuid_generate_v4() as pt_urn, uuid_generate_v4() as obj_urn, uuid_generate_v4() as method_urn
 FROM (
