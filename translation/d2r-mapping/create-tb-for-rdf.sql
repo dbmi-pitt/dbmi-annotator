5a2afe505c1ab624f63c95600fb1663216f38ba1
Yifan Ning <ningyifan1212@163.com>
2017-04-17 20:23:16 -0400

    add precipitant dose and object dose to initial RDF graph

--- translation/d2r-mapping/create-tb-for-rdf.sql
+++ translation/d2r-mapping/create-tb-for-rdf.sql
@@ -16,29 +16,27 @@ uuid_generate_v4() as p_scattered_molecular_aggregate_urn, uuid_generate_v4() as
 uuid_generate_v4() as o_scattered_molecular_aggregate_urn, uuid_generate_v4() as o_active_ingredient_urn, uuid_generate_v4() as o_drug_urn, uuid_generate_v4() as o_mass_urn, uuid_generate_v4() as o_milligram_urn
 FROM (
 WITH method AS (
-SELECT mp_claim_id, mp_data_index, m.entered_value as method from method m
-WHERE m.mp_claim_id = '1'),
+SELECT mp_claim_id, mp_data_index, m.entered_value as method from method m),
 p AS (
 SELECT ca.id, q.qvalue, q.concept_code, q.vocabulary_id, q.qualifier_role_concept_code, cb.label
 FROM mp_claim_annotation ca 
 JOIN oa_claim_body cb on cb.is_oa_body_of = ca.id
 JOIN qualifier q on q.claim_body_id = cb.id
 WHERE q.subject = True
-AND cb.label LIKE '%interact with%'
-AND ca.id = '1'),
+AND cb.label LIKE '%interact with%'),
 o AS (
 SELECT ca.id, q.qvalue, q.concept_code, q.vocabulary_id, q.qualifier_role_concept_code, cb.label
 FROM mp_claim_annotation ca 
 JOIN oa_claim_body cb on cb.is_oa_body_of = ca.id
 JOIN qualifier q on q.claim_body_id = cb.id
 WHERE q.object = True
-AND cb.label LIKE '%interact with%'
-AND ca.id = '1')
+AND cb.label LIKE '%interact with%')
 SELECT method.mp_claim_id, method.mp_data_index, method.method, p.qvalue as precipitant, p.concept_code as p_concept_code, p.qualifier_role_concept_code as p_role_concept_code, o.qvalue as object, 
 o.concept_code as o_concept_code, o.qualifier_role_concept_code as o_role_concept_code
 from method
 JOIN p ON p.id = method.mp_claim_id
 JOIN o ON o.id = method.mp_claim_id
+WHERE method.method = 'DDI clinical trial'
 ) AS tb
 
 
