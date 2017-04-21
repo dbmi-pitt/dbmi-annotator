de92b6a8f4fab5dc052e55973bba5b0622a415d9
Wen Zhang <wez61@pitt.edu>
2017-04-21 16:40:58 -0400

    merge pdf > load page problem

--- controllers/routes.js
+++ controllers/routes.js
@@ -113,6 +113,7 @@ module.exports = function(app, passport) {
     app.get('/dbmiannotator/displayWebPage', isLoggedIn, praseWebContents, function(req, res) {
 	
 	    var sourceUrl = req.query.sourceURL.trim();
+        console.log(sourceUrl);
 	    var email = req.query.email;
 	    var validUrl = require('valid-url');
 
@@ -127,8 +128,7 @@ module.exports = function(app, passport) {
 			        pluginSetL: config.profile.pluginSetL,
 			        userProfile: config.profile.userProfile
                 });   
-	        }
-		    else {
+	        } else {
 		        req.flash('loadMessage', 'The url you just entered is not valid!');
 		        res.redirect('/dbmiannotator/main');
 		    }
@@ -334,10 +334,11 @@ function getSpanFromField(field) {
 
 // parse web contents from url
 function praseWebContents(req, res, next){
+    var sourceUrl = req.query.sourceURL.trim();
 
     console.log("[DEBUG] parseWebContents");
 
-    if(sourceUrl.match(/localhost.*html/g)){
+    if(sourceUrl.match(/localhost.*html/g) || sourceUrl.match(/\.pdf/g)){
 
         if (process.env.APACHE2_HOST != null) // use apache2 in docker network
             sourceUrl = sourceUrl.replace("localhost", process.env.APACHE2_HOST);
@@ -355,7 +356,7 @@ function praseWebContents(req, res, next){
             req.htmlsource = labelDecode;
             next();                        
         });
-
+    }
 }
 
 // get plugin profile
