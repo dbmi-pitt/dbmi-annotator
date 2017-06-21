3f918c40598ea9446ebc6964afeb411051d3341d
Yifan Ning <pharmgx@gmail.com>
2017-06-21 15:30:24 -0400

    fixed bug that duplicated vocab in insert sql script

--- translation/omop-concepts-dev/data/dideo-concepts-clean.sql
+++ translation/omop-concepts-dev/data/dideo-concepts-clean.sql
@@ -1,3 +1,4 @@
 DELETE FROM public.concept WHERE concept_id BETWEEN -9999999 AND -7000000;
 DELETE FROM public.vocabulary WHERE vocabulary_concept_id BETWEEN -9999999 AND -7000000;
 DELETE FROM public.domain WHERE domain_concept_id BETWEEN -9999999 AND -7000000;
+DELETE FROM public.concept_class WHERE concept_class_concept_id BETWEEN -9999999 AND -7000000;
--- translation/omop-concepts-dev/generateDideoConceptsInsertSQL.py
+++ translation/omop-concepts-dev/generateDideoConceptsInsertSQL.py
@@ -21,7 +21,7 @@ def insert_concept_template(concept_id, concept_name, domain_id, vocabulary_id, 
     return "INSERT INTO public.concept (concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, standard_concept, concept_code, valid_start_date, valid_end_date, invalid_reason) VALUES (%s, '%s', '%s', '%s', '%s', '', '%s', '2000-01-01', '2099-02-22', '');" % (concept_id, concept_name.replace("'", "''"), domain_id, vocabulary_id, concept_class_id, concept_code)
 
 def insert_concept_class_template(concept_class_id, concept_class_name, concept_class_concept_id):
-    return "INSERT INTO concept_class (concept_class_id, concept_class_name, concept_class_concept_id) VALUES ('%s', '%s', %s);" % (concept_class_id, concept_class_name, concept_class_concept_id)
+    return "INSERT INTO public.concept_class (concept_class_id, concept_class_name, concept_class_concept_id) VALUES ('%s', '%s', %s);" % (concept_class_id, concept_class_name, concept_class_concept_id)
 
 
 def insert_vocabulary_template(vocabulary_id, vocabulary_name, vocabulary_reference, vocabulary_version, vocabulary_concept_id):
@@ -37,6 +37,10 @@ def delete_concept_by_id():
     return "DELETE FROM public.concept WHERE concept_id BETWEEN -9999999 AND -7000000;"
 
 
+def delete_concept_class_by_id():
+    return "DELETE FROM public.concept_class WHERE concept_class_concept_id BETWEEN -9999999 AND -7000000;"
+
+
 def delete_vocabulary_by_concept_id():
     return "DELETE FROM public.vocabulary WHERE vocabulary_concept_id BETWEEN -9999999 AND -7000000;"
 
@@ -49,10 +53,11 @@ def delete_domain_by_concept_id():
 # vocabulary table insert for dideo and term URI namespaces
 # return: the next available concept id 
 def print_vocabulary_insert_sql(concept_id):
-    vocabL = ['OAE', 'NCBITaxon', 'IDO', 'ERO', 'PR', 'CHMO', 'DIDEO', 'OBI', 'GO', 'DRON', 'APOLLO_SV', 'UBERON', 'CLO', 'CL', 'GO#GO', 'OGMS', 'EFO', 'STATO', 'FMA', 'CHEBI', 'MOP', 'UO', 'INO', 'PDRO.owl#PDRO']
     
     print insert_concept_template(-9999000, 'The Potential Drug-drug Interaction and Potential Drug-drug Interaction Evidence Ontology', 'Metadata', 'Vocabulary', 'Vocabulary', 'OMOP generated')    
     print insert_vocabulary_template('DIDEO', 'The Potential Drug-drug Interaction and Potential Drug-drug Interaction Evidence Ontology', 'https://github.com/DIDEO/DIDEO', 'release 2016-10-20', -9999000)
+
+    vocabL = ['OAE', 'NCBITaxon', 'IDO', 'ERO', 'PR', 'CHMO', 'OBI', 'GO', 'DRON', 'APOLLO_SV', 'UBERON', 'CLO', 'CL', 'GO#GO', 'OGMS', 'EFO', 'STATO', 'FMA', 'CHEBI', 'MOP', 'UO', 'INO', 'PDRO.owl#PDRO']
 
     for vocab in vocabL:
         print insert_concept_template(concept_id, vocab, 'Metadata', 'Vocabulary', 'Vocabulary', 'OMOP generated')
@@ -88,7 +93,7 @@ def print_domain_insert_sql():
 
 # concept class insert
 def print_concept_class_insert_sql():
-    print insert_concept_template(-9900000, 'PDDI or NPDI Test Class', 'Metadata', 'Concept Class', 'Concept Class', 'OMOP generated')    
+    print insert_concept_template(-9990000, 'PDDI or NPDI Test Class', 'Metadata', 'Concept Class', 'Concept Class', 'OMOP generated')    
     print insert_concept_class_template('PDDI or NPDI Class', 'PDDI or NPDI Test Class', -9990000)
     
 
@@ -108,8 +113,9 @@ def print_delete_script():
     print delete_concept_by_id()
     print delete_vocabulary_by_concept_id()
     print delete_domain_by_concept_id()
+    print delete_concept_class_by_id()
 
-
+    
 def main():    
     print_insert_script()
     # print_delete_script()
