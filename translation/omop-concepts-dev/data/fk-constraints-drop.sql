364ad7a745689a03fbdadda867e885fa9afa62a3
Yifan Ning <pharmgx@gmail.com>
2017-06-22 09:52:18 -0400

    add fk constraints drop sql script

---
+++ translation/omop-concepts-dev/data/fk-constraints-drop.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "public"."domain" DROP CONSTRAINT "xpk_domain";
+ALTER TABLE "public"."vocabulary" DROP CONSTRAINT "xpk_vocabulary";
+ALTER TABLE "public"."concept_relationship" DROP CONSTRAINT "xpk_concept_relationship";
+ALTER TABLE "public"."concept_class" DROP CONSTRAINT "xpk_concept_class";
+ALTER TABLE "public"."concept_ancestor" DROP CONSTRAINT "xpk_concept_ancestor";
+ALTER TABLE "public"."concept" DROP CONSTRAINT "xpk_concept";
