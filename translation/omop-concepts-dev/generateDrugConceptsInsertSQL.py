f49ae8d7acdb23f9226fa46cccabe786f7de56bb
Yifan Ning <pharmgx@gmail.com>
2017-07-31 15:59:26 -0400

    add validation to create drug concept insert script, update readme

--- translation/omop-concepts-dev/generateDrugConceptsInsertSQL.py
+++ translation/omop-concepts-dev/generateDrugConceptsInsertSQL.py
@@ -1,10 +1,9 @@
-import os, sys, csv
+import os, sys, csv, copy
 from utils import dbOperation as dop
 from utils import fileOperation as fop
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
-# reserve (-7000000, -8000000) for concept names
 
 DRUG_CSV = 'data/drug-list-mapped.csv'
 OUTPUT_SQL = 'data/drug-concepts-insert.sql'
@@ -13,11 +12,9 @@ OUTPUT_SQL = 'data/drug-concepts-insert.sql'
 CACHE = 'cache/cache-concepts-mapping.txt'
 
 # insert for drug concept into concept table
-# input: temp concept_id
-# input: sql file
-# input: concept id cache mapping file
+# input: temp concept_id, sql file, cached mapping of concept and id, used set of concept id 
 # return: the next available concept id 
-def write_concept_insert_sql(concept_id, f, cacheNameIdDict, cacheConceptIds):
+def write_concept_insert_sql(temp_concept_id, f, cacheNameIdDict, cacheConceptIds):
     reader = csv.DictReader(fop.utf_8_encoder(open(DRUG_CSV, 'r')))
     next(reader, None) # skip the header
     uniqConcepts = set() # keep unique concepts by concept name and vocabulary id
@@ -43,52 +40,57 @@ def write_concept_insert_sql(concept_id, f, cacheNameIdDict, cacheConceptIds):
             vocabulary_id = "MESH"
             concept_code = row["MESH"]
 
-        key = concept_name + vocabulary_id
+        cpt_key = vocabulary_id + ';' + concept_name
         
-        if key not in uniqConcepts:
-        
-            if row["conceptId"].strip() != "": # concept in vocab already
-                concept_id = row["conceptId"]
-            else: # add new concept
-                cpt_key = vocabulary_id + ';' + concept_name
-            
-                if cpt_key in cacheNameIdDict:
-                    concept_id = cacheNameIdDict[cpt_key]
-                else:
-                    while str(concept_id) in cacheConceptIds: # skip used concept id
-                        concept_id += 1
+        if cpt_key not in uniqConcepts and row["conceptId"].strip() == "": # concept not in vocab and not duplication, add new
+            concept_id = None
+            if cpt_key in cacheNameIdDict: # concept id defined
+                concept_id = int(cacheNameIdDict[cpt_key])
                     
-                    cacheNameIdDict[cpt_key] = concept_id # add new concept to cache
-                    cacheConceptIds.add(concept_id)
+            else: # get next temp concept id
+                while str(temp_concept_id) in cacheConceptIds: # skip used concept id
+                    temp_concept_id += 1
 
-                cpt_sql = dop.insert_concept_template(concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, concept_code, cacheNameIdDict)
+            if not concept_id:
+                concept_id = temp_concept_id
+                    
+            cpt_sql = dop.insert_concept_template(concept_id, concept_name, domain_id, vocabulary_id, concept_class_id, concept_code, cacheNameIdDict)
             f.write(cpt_sql + '\n')
-
-            uniqConcepts.add(key)
-            concept_id = int(concept_id) + 1            
             
-    return int(concept_id) + 1
+            cacheNameIdDict[cpt_key] = concept_id # add new concept to cache
+            cacheConceptIds.add(str(concept_id))
+            uniqConcepts.add(cpt_key)
+            
+    return temp_concept_id + 1
 
 
 # MAIN ###############################################################################
 def write_insert_script():
 
     # dict {'vocabId;conceptName': conceptId}
-    cacheNameIdDict = fop.readConceptCache(CACHE) # read cached concepts
-    cacheConceptIds = set(cacheNameIdDict.values()) # get concept ids that are taken
+    cacheNameIdDictBefore = fop.readConceptCache(CACHE) # read cached concepts
+    cacheNameIdDictAfter = copy.copy(cacheNameIdDictBefore) # for validate
+    cacheConceptIds = set(cacheNameIdDictAfter.values()) # get concept ids that are taken
 
-    numBefore = len(cacheNameIdDict)
-    print "[INFO] read (%s) cached concepts from (%s)" % (numBefore, CACHE)
+    print "[INFO] Read (%s) cached concepts from (%s)" % (len(cacheNameIdDictBefore), CACHE)
     
     with open(OUTPUT_SQL, 'w+') as f:
+        concept_id = -8000000 # add new terms
+        concept_id = write_concept_insert_sql(concept_id, f, cacheNameIdDictAfter, cacheConceptIds)
 
-        # add new terms
-        concept_id = -8000000
-        concept_id = write_concept_insert_sql(concept_id, f, cacheNameIdDict, cacheConceptIds)
+    ## VALIDATE ##
+    print "[SUMMARY] Added (%s) new concepts, total (%s) concepts are cached" % (len(cacheNameIdDictAfter)-len(cacheNameIdDictBefore), len(cacheNameIdDictAfter))
+    print "[VALIDATE] Check if (1) concept id not unique or not negative, (2) any existing term miss in cache file"
+    for k, v in cacheNameIdDictBefore.iteritems():            
+        if k not in cacheNameIdDictAfter or cacheNameIdDictAfter[k] != v:
+            print "[ERROR] concept term (%s) inconsistence" % k
 
-    numAfter = len(cacheNameIdDict)
-    print "[INFO] added (%s) new concepts, total (%s) concepts are cached" % (numAfter-numBefore, numAfter)
-    fop.writeConceptCache(CACHE, cacheNameIdDict) # write cached concepts
+    for k, v in cacheNameIdDictAfter.iteritems():
+        if not int(v) < 0:
+            print "[ERROR] concept term (%s) is using positive id (%s)" % (k, v)
+    
+    fop.writeConceptCache(CACHE, cacheNameIdDictAfter) # write cached concepts
+    print "[INFO] Validation done! insert script is available at (%s)" % OUTPUT_SQL
         
 def main():    
     write_insert_script()
