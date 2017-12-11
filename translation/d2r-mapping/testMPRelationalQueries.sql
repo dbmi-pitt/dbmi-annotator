5b11266fa58ff7aaddf1e52f8c1f6ff96a9a17e2
rkboyce <codehop.dev@gmail.com>
2017-12-11 17:58:16 -0500

    starting to work on getting statements into the MP graphs

--- translation/d2r-mapping/testMPRelationalQueries.sql
+++ translation/d2r-mapping/testMPRelationalQueries.sql
@@ -164,12 +164,13 @@ where negation = False
 DROP TABLE IF EXISTS ohdsi.mp_micropublication;
 CREATE TABLE ohdsi.mp_micropublication(
  id SERIAL PRIMARY KEY,
+ claim_label varchar(500),
  subj_concept_name varchar(255),
- subj_concept_id integer, 
+ subj_concept_code varchar(255), 
  subj_vocabulary_id varchar(255), 
  predicate text,
  obj_concept_name varchar(255),
- obj_concept_id integer, 
+ obj_concept_code varchar(255), 
  obj_vocabulary_id varchar(255),
  attribution_as_author varchar(255),
  attribution_as_editor varchar(255),
@@ -178,12 +179,13 @@ CREATE TABLE ohdsi.mp_micropublication(
 );
 
 INSERT INTO ohdsi.mp_micropublication 
-   (subj_concept_name, subj_concept_id, subj_vocabulary_id, predicate, obj_concept_name, obj_concept_id, obj_vocabulary_id,
+   (claim_label, subj_concept_name, subj_concept_code, subj_vocabulary_id, predicate, obj_concept_name, obj_concept_code, obj_vocabulary_id,
    attribution_as_author, attribution_as_editor, attribution_as_publisher, attribution_as_curator)
-SELECT qsc_c.concept_name, qsc_c.concept_id, qsc_c.vocabulary_id, qpv, qoc_c.concept_name, qoc_c.concept_id, qoc_c.vocabulary_id,
+SELECT qsc_c.concept_name || ' (' || qsc_c.concept_code || '|' || qsc_c.vocabulary_id || ') ' || '<' || qpv || '>' || ' ' || qoc_c.concept_name || ' (' || qoc_c.concept_code || '|' ||  qoc_c.vocabulary_id || ')' as claim_label, 
+       qsc_c.concept_name, qsc_c.concept_code, qsc_c.vocabulary_id, qpv, qoc_c.concept_name, qoc_c.concept_code, qoc_c.vocabulary_id,
       'https://orcid.org/0000-0002-2993-2085', 'https://orcid.org/0000-0002-2993-2085','https://orcid.org/0000-0002-2993-2085','https://orcid.org/0000-0002-2993-2085' 
 FROM (
-select qsubj.claim_body_id claimid, 
+select qsubj.claim_body_id claim_body_id, 
        qsubj.qvalue qsv, 
        qpred.qvalue qpv, 
        qobj.qvalue qov, 
@@ -205,6 +207,66 @@ where qsubj.subject = True
      inner join public.vocabulary qovv on qoc_c.vocabulary_id = qovv.vocabulary_id
   where qsvid = qsvv.vocabulary_concept_id
     and qovid = qovv.vocabulary_concept_id
-group by qsc_c.concept_name, qsc_c.concept_id, qsc_c.vocabulary_id, qpv, qoc_c.concept_name, qoc_c.concept_id, qoc_c.vocabulary_id
-order by qsc_c.concept_name, qsc_c.concept_id, qsc_c.vocabulary_id, qpv, qoc_c.concept_name, qoc_c.concept_id, qoc_c.vocabulary_id
+group by qsc_c.concept_name, qsc_c.concept_code, qsc_c.vocabulary_id, qpv, qoc_c.concept_name, qoc_c.concept_code, qoc_c.vocabulary_id
+order by qsc_c.concept_name, qsc_c.concept_code, qsc_c.vocabulary_id, qpv, qoc_c.concept_name, qoc_c.concept_code, qoc_c.vocabulary_id
 ;
+
+---------------------------------
+
+SELECT c_code
+  FROM
+  (SELECT id, subj_concept_code c_code FROM ohdsi.mp_micropublication ORDER BY id ASC) t1
+  UNION ALL
+   SELECT c_code 
+   FROM (SELECT id, obj_concept_code c_code FROM ohdsi.mp_micropublication ORDER BY id ASC) t2
+ UNION ALL
+   SELECT c_code
+   FROM (SELECT id, predicate c_code FROM ohdsi.mp_micropublication ORDER BY id ASC) t3
+;
+
+DROP TABLE IF EXISTS ohdsi.claim_semantic_qualifiers;
+CREATE TABLE ohdsi.claim_semantic_qualifiers
+(
+ id SERIAL PRIMARY KEY,
+ concept_code varchar(255),
+ concept_name varchar(255),
+ vocabulary_id varchar(255),
+ qualifies integer
+ );
+
+INSERT INTO ohdsi.claim_semantic_qualifiers (concept_code, concept_name, vocabulary_id, qualifies)  
+  SELECT c_code, concept_name, vocabulary_id, id 
+  FROM (
+    SELECT c_code, concept_name, vocabulary_id, id FROM (
+       SELECT id, subj_concept_code c_code, subj_concept_name concept_name, subj_vocabulary_id vocabulary_id
+       FROM ohdsi.mp_micropublication ORDER BY id ASC) t1   
+  UNION ALL    
+     SELECT c_code, concept_name, vocabulary_id, id FROM (
+       SELECT id, obj_concept_code c_code, obj_concept_name concept_name, obj_vocabulary_id vocabulary_id
+       FROM ohdsi.mp_micropublication ORDER BY id ASC) t1  
+  UNION ALL
+      SELECT c_code, c_code concept_name, 'dikb' vocabulary_id, id FROM (
+       SELECT id, predicate c_code 
+       FROM ohdsi.mp_micropublication ORDER BY id ASC) t3) t4;
+
+select qualifies from ohdsi.claim_semantic_qualifiers;      
+
+-------------
+
+-- Set up a table that will help create the MP:Statement resources that support/challenge claims
+SELECT oas.exact,
+       cb.id, cb.urn, cb.label, 
+       mp.*, 
+       ca.*      
+FROM 
+ohdsi.oa_claim_body cb inner join ohdsi.qualifier qsubj on cb.id = qsubj.claim_body_id
+ inner join ohdsi.qualifier qpred on qsubj.claim_body_id = qpred.claim_body_id
+ inner join ohdsi.qualifier qobj on qsubj.claim_body_id = qobj.claim_body_id
+ inner join ohdsi.mp_micropublication mp on mp.subj_concept_code = qsubj.concept_code 
+    and mp.obj_concept_code = qobj.concept_code 
+    and mp.predicate = qpred.qvalue
+ inner join ohdsi.mp_claim_annotation ca on cb.id = ca.has_body
+ inner join ohdsi.oa_target oat on ca.has_target = oat.id
+ inner join ohdsi.oa_selector oas on oat.has_selector = oas.id
+;
+
