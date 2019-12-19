d2a482461c469d6dd7de3b6e1a6d0cf7536fd28b
maxsibilla <maxsibilla@icloud.com>
2019-12-19 11:12:53 -0500

    Updating dependecies in project to resolve security issues in old version of sequelize. Upgrade user model to support new version of sequelize

--- models/user.js
+++ models/user.js
@@ -12,37 +12,36 @@
  See the License for the specific language governing permissions and
  limitations under the License. */
 
-var bcrypt   = require('bcryptjs');
- 
-module.exports = function(sequelize, Sequelize) {
-    var User = sequelize.define('user', {
-	id: {type: Sequelize.INTEGER, unique: true, primaryKey: true, autoIncrement: true} ,
-	uid: Sequelize.TEXT,
-	username: Sequelize.TEXT,
-	admin: Sequelize.BOOLEAN,
-	manager: Sequelize.BOOLEAN,
-	email: Sequelize.TEXT,
-	status: Sequelize.INTEGER,
-	last_login_date: Sequelize.DATE,
-	registered_date: Sequelize.DATE,
-	activation_id: Sequelize.INTEGER,
-	password: Sequelize.TEXT
-    },
-				{classMethods:
-				 {
-				     validPassword: function(password1, password2){
-			    		 return bcrypt.compareSync(password1, password2);
-				     },
-				     generateHash: function(password){
-			    		 return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
-				     }
-				 }
-				}			     
-				
-			       );
-    
+var bcrypt = require('bcryptjs');
+
+module.exports = function (sequelize, Sequelize) {
+    const User = sequelize.define('user', {
+        id: {type: Sequelize.INTEGER, unique: true, primaryKey: true, autoIncrement: true},
+        uid: Sequelize.TEXT,
+        username: Sequelize.TEXT,
+        admin: Sequelize.BOOLEAN,
+        manager: Sequelize.BOOLEAN,
+        email: Sequelize.TEXT,
+        status: Sequelize.INTEGER,
+        last_login_date: Sequelize.DATE,
+        registered_date: Sequelize.DATE,
+        activation_id: Sequelize.INTEGER,
+        password: Sequelize.TEXT
+    }, {});
+
+    User.associate = function (models) {
+    };
+
+    User.validPassword = function (password1, password2) {
+        return bcrypt.compareSync(password1, password2);
+    };
+    User.generateHash = function (password) {
+        return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+    };
+
     return User;
-}
+};
+
 
 
 
