a7fdce577eaccc31d9104d145ea22756d2fa769c
mas400 <mas400@mas400-mdt.dbmi.pitt.edu>
2020-03-10 13:46:28 -0400

    Updating user and package to use newer versions of elasticsearch and other packages

---
+++ models/olduser.js
@@ -0,0 +1,49 @@
+/* Copyright 2016-2017 University of Pittsburgh
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http:www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License. */
+
+var bcrypt   = require('bcryptjs');
+
+module.exports = function(sequelize, Sequelize) {
+    var User = sequelize.define('user', {
+	id: {type: Sequelize.INTEGER, unique: true, primaryKey: true, autoIncrement: true} ,
+	uid: Sequelize.TEXT,
+	username: Sequelize.TEXT,
+	admin: Sequelize.BOOLEAN,
+	manager: Sequelize.BOOLEAN,
+	email: Sequelize.TEXT,
+	status: Sequelize.INTEGER,
+	last_login_date: Sequelize.DATE,
+	registered_date: Sequelize.DATE,
+	activation_id: Sequelize.INTEGER,
+	password: Sequelize.TEXT
+    },
+				{classMethods:
+				 {
+				     validPassword: function(password1, password2){
+			    		 return bcrypt.compareSync(password1, password2);
+				     },
+				     generateHash: function(password){
+			    		 return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+				     }
+				 }
+				}
+
+			       );
+
+    return User;
+}
+
+
+
+
--- models/user.js
+++ models/user.js
@@ -45,4 +45,3 @@ module.exports = function (sequelize, Sequelize) {
 
 
 
-
